5473e6b6117bf6a6e6b8a0005afab189013500ae
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2019-12-05 13:06:44 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -5,5 +5,19 @@ load "./local_env.rb" if File.exists?("./local_env.rb")
 
 get '/' do
 puts "hi"
-    return "hi"
+    erb :index
+end
+
+post '/contact' do
+puts "yo"
+    firstname = params[:firstname]
+    lastname = params[:lastname]
+    name = "#{firstname} #{lastname}"
+    email = params[:email]
+    phone = params[:phone]
+    message = params[:message]
+
+    send_email('Thank You For Contacting Me.', {name: name, phone: phone, email: email, message: message})
+redirect '/'
+
 end
