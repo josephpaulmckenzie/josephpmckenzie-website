7107e1f97ed04da12764009e6849d81782a17af0
Joseph Mckenzie <joseph.mckenzie@poetic.io>
2020-02-12 19:01:43 -0500

    changing mail up

--- app.rb
+++ app.rb
@@ -9,26 +9,28 @@ def email_options()
     :address => 'smtp.gmail.com',
     :port => '587',
     :enable_starttls_auto => true,
-    :user_name =>ENV['from'],
-    :password => ENV["password"],
-    :authentication => :plain, 
-    :domain => ENV['domain']
+    :user_name =>"joseph.p.mckenzie84@gmail.com",
+    :password => "Bubbadog1907",
+    :authentication => 'plain', 
+    :domain => "josephpmckenzie.com"
     }
 end
 
 def send_email(title, details)
+    puts "in send email"
 	begin
 puts "email"
 		Pony.mail(
-		  :to => details[:email],
+		  :to =>"josephmckenzieaz@yahoo.com",
 		  :cc => "joseph.p.mckenzie84@gmail.com",
-		  :from => ENV["from"],
+		  :from =>"joseph.p.mckenzie84@gmail.com",
 		  :subject => "Hello from Joseph Mckenzie", 
 		  :content_type => 'text/html', 
 		  :body => erb(:email,:layout=>false, locals: {details: details, title: title}),
 		  :via => :smtp, 
 		  :via_options => email_options()
-		)
+        )
+        puts "HEY"
 	rescue  => error
 		puts "Error sending email #{error}"
 	end
